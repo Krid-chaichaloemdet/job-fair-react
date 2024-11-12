654138ed9103b537701dacf72fc6971c13d60874
Krid-Chaichaloemdet <kridtue01@hotmail.com>
2024-11-12 10:08:15 +0700

    register form

--- src/component/registerComponent/RegisterInput.jsx
+++ src/component/registerComponent/RegisterInput.jsx
@@ -1,20 +1,34 @@
 import React from "react";
-
+import axios from 'axios'
 export default function RegisterInput({
   name,
   typeOfInput = "text",
   amout,
-  radio1,
-  radio2,
-  radio3,
-  value
+  value,
+  textArea,
+  textAreaHight,
+  textAreaWidth,
 }) {
   const inputs = [];
+
+  const typeOfGender = [
+    { title: "male" },
+    { title: "female" },
+    { title: "other" },
+  ];
 
   if (amout > 1) {
     for (let i = 0; i < amout; i++) {
       inputs.push(
-        <input value={`radio${i + 1}`} key={i} name={name} type={typeOfInput} />
+        <div className="flex gap-2">
+          <input
+            value={typeOfGender[i]?.title}
+            key={i}
+            name={name}
+            type={typeOfInput}
+          />
+          <label htmlFor="">{typeOfGender[i]?.title}</label>
+        </div>
       );
     }
   }
@@ -26,7 +40,15 @@ export default function RegisterInput({
         inputs
       ) : (
         <>
-          <input type="text" name={name}/>
+          {textArea ? (
+            <textarea
+            rows={4}
+              className={`resize-none ${textAreaWidth} rounded-sm`}
+              name={name} 
+            ></textarea>
+          ) : (
+            <input className="rounded-md" type={typeOfInput} name={name} />
+          )}
         </>
       )}
     </div>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import RegisterInput from "../component/registerComponent/RegisterInput";
 import RegisterLabel from "../component/registerComponent/RegisterLabel";
+import axios from "axios";
 export default function RegisterPage() {
   const [input, setInput] = useState({
     name: "",
@@ -8,34 +9,69 @@ export default function RegisterPage() {
     dateOfBirth: "",
     gender: "",
     phoneNumber: "",
+    email: '',
+    education: '',
+    faculty: '',
+    department : '',
+    dateCanStartWorking :'',
+
   });
 
   const handleInput = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
   const arr = [
-    { id: 1, title: "First Name - Last Name", name: "name" , typeOfInput: "radio", },
+    {
+      id: 1,
+      title: "First Name - Last Name",
+      name: "name",
+    },
     {
       id: 2,
       title: "Address",
       name: "address",
+      typeOfInput: '',
+      textArea : true,
+      textAreaHight: 'h-10',
+      textAreaWidth: 'w-2/5'
+    },
+    { id: 3, title: "Date of birth MM/DD/YYYY", name: "dateOfBirth" , typeOfInput: 'date'},
+    {
+      id: 4,
+      title: "Gender",
+      name: "gender",
       typeOfInput: "radio",
       amout: 3,
-      radio1: "one",
-      radio2: "two",
-      radio3: "tree",
+      radio1: "male",
+      radio2: "female",
+      radio3: "other",
     },
-    { id: 3, title: "Date Of Birth", name: "dateOfBirth" },
-    { id: 4, title: "Gender", name: "gender" },
     { id: 5, title: "Phone Number", name: "phoneNumber" },
-    { id: 6, title: "Phone Number", name: "phoneNumber" },
+    { id: 6, title: "E-mail", name: "email" },
+    { id: 7, title: "Education", name: "education" },
+    { id: 8, title: "Faculty", name: "faculty" },
+    { id: 9, title: "Department", name: "department" },
+    { id: 10, title: "Date you can start working MM/DD/YYYY", name: "dateCanStartWorking", typeOfInput: 'date' },
   ];
   console.log(input);
+
+  const handleSubmitForm =  async(e) =>{
+    try {
+      e.preventDefault()
+      axios.post('http://localhost:8000/user/register', input )
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div className="w-screen h-screen bg-red-300 p-10 flex flex-col gap-2">
+    <form 
+    onSubmit={handleSubmitForm}
+    className="w-screen h-screen bg-blue-500 p-10 flex flex-col gap-2">
       {arr.map((data, i) => {
         return (
-          <div onChange={handleInput} key={data.id}>
+          <div 
+          className="flex flex-col gap-1 w-full "
+          onChange={handleInput} key={data.id}>
             <RegisterLabel label={arr[i].title} />
             <RegisterInput
               name={arr[i].name}
@@ -45,10 +81,14 @@ export default function RegisterPage() {
               radio1={arr[i].radio1}
               radio2={arr[i].radio2}
               radio3={arr[i].radio3}
+              textArea={arr[i].textArea}
+              textAreaHight={arr[i].textAreaHight}
+              textAreaWidth={arr[i].textAreaWidth}
             />
           </div>
         );
       })}
-    </div>
+      <button className="bg-green-300">Submit</button>
+    </form>
   );
 }
