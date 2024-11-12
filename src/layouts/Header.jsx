85099a705f6d7882904c2ab8e39405f398f0fc07
charin.go <got171143@gmail.com>
2024-11-12 11:49:25 +0700

    add header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import evalogo from '../../public/logo/evaLogo.svg'
+import kcilogo from '../../public/logo/kcilogo.svg'
+import meworxlogo from '../../public/logo/meworxlogo.svg'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='bg-[#131E3C] flex flex-row justify-center items-center py-[1.5rem]'>
+      <img src={evalogo} className='mx-[2%] h-[2rem]' alt="" />
+      <img src={kcilogo} className='mx-[2%] h-[2rem]' alt='' />
+      <img src={meworxlogo} className='mx-[2%] h-[1.35rem]' alt='' />
+    </div>
   )
 }
