1e0cdc1196bf64d952b6fb28288c824d75a1c063
charin.go <got171143@gmail.com>
2024-11-13 17:06:22 +0700

    update jobs page

--- src/component/homePageComponent/PositionData.jsx
+++ src/component/homePageComponent/PositionData.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function PositionData({
   onClose,
@@ -7,43 +7,71 @@ export default function PositionData({
   requirement,
 }) {
   console.log("jobDes", jobDes);
+
+  const [openModal, setOpenModal] = useState(false);
+
+  useEffect(() => {
+    setOpenModal(true);
+  }, []);
+
+  const handleClose = () => {
+    setOpenModal(false);
+    setTimeout(() => {
+      onClose(false);
+    }, 300);
+  };
+
+
   return (
-    <div className="fixed inset-0 bg-[#131E3c] bg-opacity-50 flex items-end z-50">
-      <div className="bg-white rounded-[20px] w-full h-[80%] p shadow-lg relative">
+    <div className={`fixed inset-0 bg-[#131E3c] flex items-end z-50 duration-200 ease-in
+        ${
+          openModal ? "bg-opacity-50" : "bg-opacity-0"
+        }`}>
+      <div className={`bg-white rounded-t-[20px] w-full h-[80%] relative duration-300 flex flex-col justify-start transform 
+        ${
+          openModal ? "translate-y-0" : "translate-y-full"
+        }`}
+      >
         <img
-          onClick={() => onClose(false)}
+          onClick={handleClose}
           src={"src/assets/close-circle.png"}
-          className="absolute top-2 right-2  w-[1.5rem] z-100"
+          className="absolute top-3 right-3 w-[1.75rem] z-100 cursor-pointer rounded-full"
         ></img>
-        <div className="bg-[#131E3c] rounded-[20px] h-[100px] w-full flex justify-center items-center ">
-          <h1 className="text-center text-[#fff] text-[14px]">
+        <div className="flex bg-[#131E3c] rounded-[20px] min-h-[140px] w-full  justify-center items-center ">
+          <div className="text-center text-[#fff] text-[1rem] tracking-[0.2em] font-medium">
             {positionName}
-          </h1>
+          </div>
         </div>
 
-        <div className="flex flex-col pl-5 w-full h-[65%] overflow-y-scroll">
-          <h3 className="font-bold text-[14px] py-2">Job Description</h3>
-          <ul className="list-disc text-[12px] pl-5">
-            {
-              // eslint-disable-next-line react/prop-types, no-undef
-              jobDes.map((data, i) => {
-                console.log(data);
-                return <li key={i}>{data.title}</li>;
-              })
-            }
-          </ul>
-          <h3 className="font-bold text-[14px] py-2">Requirements</h3>
-          <ul className="list-disc text-[12px] pl-5">
-            {
-              // eslint-disable-next-line react/prop-types
-              requirement.map((data, i) => {
-                return <li key={i}>{data.title}</li>;
-              })
-            }
-          </ul>
+        <div className="flex flex-col px-[1.5rem] py-[0.5rem] w-full h-full overflow-y-scroll">
+          <div className="my-[0.5rem]">
+            <div className="font-medium text-[1rem] py-[0.75rem] tracking-[0.1em]">Job Description</div>
+            <ul className="list-disc text-[12px] pl-5">
+              {
+                // eslint-disable-next-line react/prop-types, no-undef
+                jobDes.map((data, i) => {
+                  console.log(data);
+                  return <li key={i}>{data.title}</li>;
+                })
+              }
+            </ul>
+          </div>
+          <div className="my-[0.5rem]">
+            <div className="font-medium text-[1rem] py-[0.75rem] tracking-[0.1em]">Requirements</div>
+            <ul className="list-disc text-[12px] pl-5">
+              {
+                // eslint-disable-next-line react/prop-types
+                requirement.map((data, i) => {
+                  return <li key={i}>{data.title}</li>;
+                })
+              }
+            </ul>
+          </div>
+          
+          
         </div>
         <div className="py-5 flex justify-center items-center">
-          <button className="bg-[#131E3c] rounded-[20px] h-[40px] w-52 text-white text-[14px]">
+          <button className="bg-[#131E3c] rounded-full py-[0.75rem] w-[80%] text-white text-[0.9rem] tracking-[0.2em]">
             Apply for Job
           </button>
         </div>
