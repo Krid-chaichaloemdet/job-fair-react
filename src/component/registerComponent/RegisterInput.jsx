51c4b3fc3e8f0e3a553b26199bae36ba1e78ffb7
charin.go <got171143@gmail.com>
2024-11-13 10:20:29 +0700

    add footer

--- src/component/registerComponent/RegisterInput.jsx
+++ src/component/registerComponent/RegisterInput.jsx
@@ -10,6 +10,7 @@ export default function RegisterInput({
   textAreaWidth,
   isDropDown,
   positionDropDown,
+  
 }) {
 
   const [isOpenPositionDropDown, setIsOpenPositionDropDown] = useState(false);
