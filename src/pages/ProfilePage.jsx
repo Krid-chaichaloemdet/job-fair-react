d6bcfff49d171cde791832e697326dad63991730
oillyPratoo <Jenjira4564@gmail.com>
2024-11-19 11:14:19 +0700

    profilebyoil

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 
 export default function ProfilePage() {
   return (
-    <div>ProfilePage</div>
+    <div className='w-full h-[100%] px-[2rem] py-[1rem] mt-[80px] flex flex-col mb-[6rem]'>
+      <div className='flex flex-row items-start justify-start'>
+        <h1 className='font-medium text-[1.75rem] tracking-[0.1em]'>Profile</h1>
+      </div>
+      <div className='flex flex-col justify-center w-full py-2'>
+        <div className='py-2'>
+          <p className='pb-2'>Your first name</p>
+          <input class="rounded-md bg-[#131e3c13] w-[100%] h-10 border-[1px] border-[#131e3c33] py-[0.25rem] px-[0.25rem] z-0" type="text" name="firstName" fdprocessedid="6kf01q"></input>
+        </div>
+        <div className='py-2'>
+          <p className='pb-2'>Your last name</p>
+          <input class="rounded-md bg-[#131e3c13] w-[100%] h-10 border-[1px] border-[#131e3c33] py-[0.25rem] px-[0.25rem] z-0 " type="text" name="lastName" fdprocessedid="6kf01q"></input>
+        </div>
+        <div className='py-2'>
+          <p className='pb-2'>Phone number</p>
+          <input class="rounded-md bg-[#131e3c13] w-[100%] h-10 border-[1px] border-[#131e3c33] py-[0.25rem] px-[0.25rem] z-0 " type="number" name="phonenumber" fdprocessedid="6kf01q"></input>
+        </div>
+        <div className='py-2'>
+          <p className='pb-2'>Email</p>
+          <input class="rounded-md bg-[#131e3c13] w-[100%] h-10 border-[1px] border-[#131e3c33] py-[0.25rem] px-[0.25rem] z-0 " type="email" name="email" fdprocessedid="6kf01q"></input>
+        </div>
+      </div>
+      <button class="bg-[#131E3C] rounded-full py-[0.75rem] my-[0.5rem]" fdprocessedid="rfa0wpn"><div class="text-[#ffffff] tracking-[0.12em]">Edit Profile</div></button>
+    </div>
   )
 }
